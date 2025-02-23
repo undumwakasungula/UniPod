41541caaa2f8d927ad6ea27ffe8e05c6481f50e9
undumwakasungula <beee22-umpeta@mubas.ac.mw>
2025-02-23 20:59:21 +0200

    a couple of changes

--- main.js
+++ main.js
@@ -15,10 +15,36 @@ function resize(e) {
     const newWidth = e.clientX - leftDiv.getBoundingClientRect().left;
     leftDiv.style.width = `${newWidth}px`;
   }
-}
+};
 
 function stopResize() {
   isResizing = false;
   document.removeEventListener('mousemove', resize);
   document.removeEventListener('mouseup', stopResize);
-}
+};
+
+const statusparagraph = document.getElementById("stat");
+const statushide = document.getElementById("statushidden");
+const addparagraph = document.getElementById("add");
+const addhide = document.getElementById("addhidden");
+
+
+statusparagraph.addEventListener("click", function statusEquipment(){
+  
+  if(statushide.style.visibility==="hidden"){
+    statushide.style.visibility= "visible";
+  }
+  if(statushide.style.visibility==="visible"){
+    addhide.style.visibility= "hidden";
+  }
+} );
+
+
+addparagraph.addEventListener("click", function addEquipmentt(){
+  if(addhide.style.visibility==="hidden"){
+    addhide.style.visibility= "visible";
+  }
+  if(addhide.style.visibility==="visible"){
+    statushide.style.visibility= "hidden";
+  }
+} );
