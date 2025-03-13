f5028fc34d18c33cbbe4ca612b464cd73ec35d6c
undumwakasungula <beee22-umpeta@mubas.ac.mw>
2025-03-13 23:48:51 +0100

    database integration
    firebase intergration

---
+++ firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAuVPMEyPKqEuic6uSV48AUycDald94Gqo",
+  authDomain: "unipod-management.firebaseapp.com",
+  projectId: "unipod-management",
+  storageBucket: "unipod-management.firebasestorage.app",
+  messagingSenderId: "347626598563",
+  appId: "1:347626598563:web:7f2ce85d849bad96830f16",
+  measurementId: "G-0LCMERXLWZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+        
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+
+// Initialize Firebase Authentication
+const auth = getAuth();
+
+export {auth};
---
+++ public/form.js
@@ -0,0 +1,68 @@
+import { auth } from "../firebase.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+document.addEventListener("DOMContentLoaded", function() {
+    const upform = document.getElementById("signUpform");
+    const subform = document.getElementById("signIn-form");
+    const button_activity = document.getElementById("sign-up-label");
+    
+    if (upform) {
+        upform.addEventListener("submit", async (event) => {
+            
+            event.preventDefault();
+            let email = document.getElementById("email").value;
+            let password = document.getElementById("password").value.trim();
+            let confirm_password = document.getElementById("confPassword").value.trim();
+            try {
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                console.log("User created:", userCredential.user);
+                if (password === confirm_password) {
+                    alert("Account created successfully!");
+                    window.location.href = "studentLogin.html";
+                } else {
+                    alert("Passwords are not a match");
+                }
+              } catch (error) {
+                console.error("Error creating account:", error.message);
+                alert("Error: " + error.message);
+            }
+
+        });
+    }
+
+    if (subform) {
+        subform.addEventListener("submit", async (event) => {
+            event.preventDefault();
+
+            const email = document.getElementById("email").value;
+            const password1 = document.getElementById("password").value;
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                console.log("Signed in successfully:", userCredential.user);
+                window.location.href = "main.html";
+              } catch (error) {
+                console.error("Error signing in:", error.message);
+                alert("Error: " + error.message);
+              }
+        });
+    }
+
+    if (button_activity) {
+        button_activity.addEventListener("click", function ghost() {
+            window.location.href = "signUp_selector.html";
+        });
+    }
+
+
+});
+function gotoStudent() {
+    window.location.href = "StudentSignup.html";
+};
+
+function gotoStaff() {
+    window.location.href = "staffSignup.html";
+};
+
+function gotoExternal() {
+    window.location.href = "externalsignup.html";
+};
