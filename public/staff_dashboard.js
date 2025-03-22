c39a1451f655a22bb574b4a659df721c73feacb5
undumwakasungula <beee22-umpeta@mubas.ac.mw>
2025-03-22 20:30:54 +0100

    just push

--- public/staff_dashboard.js
+++ public/staff_dashboard.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const left_main = document.getElementById("left-main");
     const reception = document.getElementById("reception");
     const workshop = document.getElementById("workshop");
+    const sub_workshop = document.getElementById("workshop_subsection");
     const addReceptionist = document.getElementById("reception_id");
     const addWorkshop = document.getElementById("workshop_id");
     const computer_btn = document.getElementById("compu");
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const wood_section = document.getElementById("wood");
     const cnc_section = document.getElementById("cnc");
     const paragraphs = document.querySelectorAll(".paragraph_select");
+    const sub_paragraphs = document.querySelectorAll(".paragraph_selection");
     const welcome_message = document.getElementById("welcome_remark");
     const DCIO_btn = document.getElementById("DCIO_btn");
     const DCIO_section = document.getElementById("DCIO");
@@ -28,11 +30,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const Booking_section = document.getElementById("Booking");
     const Membership_btn = document.getElementById("Membership_btn");
     const Membership_section = document.getElementById("Membership");
+    //equipment and projects
+    const compu_equip = document.getElementById("compu-equip");
+    const audio_equip = document.getElementById("audio-equip");
+    const tronics_equip = document.getElementById("tronics-equip");
+    const mecha_equip = document.getElementById("mech-equip");
+    const wood_equip = document.getElementById("wood-equip");
+    const cnc_equip = document.getElementById("cnc-equip");
+    const audio_projects = document.getElementById("audio-projects");
+    const tronics_projects = document.getElementById("tronics-projects");
+    const mecha_projects = document.getElementById("mech-projects");
+    const wood_projects = document.getElementById("wood-projects");
+    const cnc_projects = document.getElementById("cnc-projects");
   if(reception){
     reception.addEventListener("click", function getreception(){
         const elements = document.querySelectorAll(".content-windows");
         const elements_b = document.querySelectorAll(".windows");
-    
+        if(sub_workshop.style.display==="block"){
+          sub_workshop.style.display= "none";
+      }
         if(left_main.style.visibility === "hidden"){
             
         left_main.style.visibility = "visible";
@@ -62,6 +78,9 @@ document.addEventListener("DOMContentLoaded", function() {
     workshop.addEventListener("click", function getworkshop(){
       const elements = document.querySelectorAll(".content-windows");
       const elements_b = document.querySelectorAll(".windows");
+        if(sub_workshop.style.display==="none"){
+          sub_workshop.style.display= "block";
+        }
         if(left_main.style.visibility === "hidden"){
             left_main.style.visibility = "visible";
         }
@@ -83,6 +102,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   if(computer_btn){
     computer_btn.addEventListener("click", function getcomputer(){
+
         if(welcome_message.style.display==="block"){
           welcome_message.style.display= "none";
         }
@@ -95,6 +115,21 @@ document.addEventListener("DOMContentLoaded", function() {
             mechanical_section.style.display= "none";
             wood_section.style.display= "none";
             cnc_section.style.display= "none";
+        }
+        if(compu_equip.style.display==="none"){
+            compu_equip.style.display= "block";
+        }
+        if(compu_equip.style.display==="block"){
+            audio_equip.style.display= "none";
+            tronics_equip.style.display= "none";
+            mecha_equip.style.display= "none";
+            wood_equip.style.display= "none";
+            cnc_equip.style.display= "none";
+            audio_projects.style.display= "none";
+            tronics_projects.style.display= "none";
+            mecha_projects.style.display= "none";
+            wood_projects.style.display= "none";
+            cnc_projects.style.display= "none";
         }
       });
   }
@@ -113,6 +148,23 @@ document.addEventListener("DOMContentLoaded", function() {
             wood_section.style.display= "none";
             cnc_section.style.display= "none";
         }
+        if(audio_projects.style.display==="none" && audio_equip.style.display==="none"){
+          audio_equip.style.display= "block";  
+          audio_projects.style.display= "block";
+            
+        }
+        if(audio_projects.style.display==="block" && audio_equip.style.display==="block"){
+            tronics_projects.style.display= "none";
+            mecha_projects.style.display= "none";
+            wood_projects.style.display= "none";
+            cnc_projects.style.display= "none";
+            compu_equip.style.display= "none";
+            tronics_equip.style.display= "none";
+            mecha_equip.style.display= "none";
+            wood_equip.style.display= "none";
+            cnc_equip.style.display= "none";
+        }
+
       });
   }
   if(electronics_btn){
@@ -129,7 +181,25 @@ document.addEventListener("DOMContentLoaded", function() {
             mechanical_section.style.display= "none";
             wood_section.style.display= "none";
             cnc_section.style.display= "none";
+            
         }
+        if(tronics_equip.style.display==="none" && tronics_projects.style.display==="none"){
+            tronics_equip.style.display= "block";
+            tronics_projects.style.display= "block";
+        }
+        if(tronics_equip.style.display==="block" && tronics_projects.style.display==="block"){
+            compu_equip.style.display= "none";
+            audio_equip.style.display= "none";
+            mecha_equip.style.display= "none";
+            wood_equip.style.display= "none";
+            cnc_equip.style.display= "none";
+            audio_projects.style.display= "none";
+            mecha_projects.style.display= "none";
+            wood_projects.style.display= "none";
+            cnc_projects.style.display= "none";
+            
+        }
+
       });
   }
   if(mechanical_btn){
@@ -147,6 +217,55 @@ document.addEventListener("DOMContentLoaded", function() {
             wood_section.style.display= "none";
             cnc_section.style.display= "none";
         }
+        if(mecha_equip.style.display==="none" && mecha_projects.style.display==="none"){
+            mecha_equip.style.display= "block";
+            mecha_projects.style.display= "block";
+        }
+        if(mecha_equip.style.display==="block" && mecha_projects.style.display==="block"){
+            compu_equip.style.display= "none";
+            audio_equip.style.display= "none";
+            tronics_equip.style.display= "none";
+            wood_equip.style.display= "none";
+            cnc_equip.style.display= "none";
+            audio_projects.style.display= "none";
+            tronics_projects.style.display= "none";
+            wood_projects.style.display= "none";
+            cnc_projects.style.display= "none";
+        }
+
+      });
+  }
+  if(cnc_btn){
+    cnc_btn.addEventListener("click", function getcnc(){
+        if(welcome_message.style.display==="block"){
+          welcome_message.style.display= "none";
+        }
+        if(cnc_section.style.display==="none"){
+            cnc_section.style.display= "block";
+        }
+        if(cnc_section.style.display==="block"){
+            computer_section.style.display= "none";
+            audio_section.style.display= "none";
+            electronics_section.style.display= "none";
+            mechanical_section.style.display= "none";
+            wood_section.style.display= "none";
+        }
+        if(cnc_equip.style.display==="none" && cnc_projects.style.display==="none"){
+            cnc_equip.style.display= "block";
+            cnc_projects.style.display= "block";
+        }
+        if(cnc_equip.style.display==="block" && cnc_projects.style.display==="block"){
+            compu_equip.style.display= "none";
+            audio_equip.style.display= "none";
+            tronics_equip.style.display= "none";
+            mecha_equip.style.display= "none";
+            wood_equip.style.display= "none";
+            audio_projects.style.display= "none";
+            tronics_projects.style.display= "none";
+            mecha_projects.style.display= "none";
+            wood_projects.style.display= "none";
+        }
+
       });
   }
   if(wood_btn){
@@ -165,25 +284,25 @@ document.addEventListener("DOMContentLoaded", function() {
             mechanical_section.style.display= "none";
             cnc_section.style.display= "none";
         }
+        if(wood_equip.style.display==="none" && wood_projects.style.display==="none"){
+            wood_equip.style.display= "block";
+            wood_projects.style.display= "block";
+        }
+        if(wood_equip.style.display==="block" && wood_projects.style.display==="block"){
+            compu_equip.style.display= "none";
+            audio_equip.style.display= "none";
+            tronics_equip.style.display= "none";
+            mecha_equip.style.display= "none";
+            cnc_equip.style.display= "none";
+            audio_projects.style.display= "none";
+            tronics_projects.style.display= "none";
+            mecha_projects.style.display= "none";
+            cnc_projects.style.display= "none";
+        }
+
       });
   }
-  if(cnc_btn){
-    cnc_btn.addEventListener("click", function getcnc(){
-        if(welcome_message.style.display==="block"){
-          welcome_message.style.display= "none";
-        }
-        if(cnc_section.style.display==="none"){
-            cnc_section.style.display= "block";
-        }
-        if(cnc_section.style.display==="block"){
-            computer_section.style.display= "none";
-            audio_section.style.display= "none";
-            electronics_section.style.display= "none";
-            mechanical_section.style.display= "none";
-            wood_section.style.display= "none";
-        }
-      });
-  }
+
   if(DCIO_btn){
     DCIO_btn.addEventListener("click", function getDCIO(){
         if(welcome_message.style.display==="block"){
@@ -250,6 +369,16 @@ document.addEventListener("DOMContentLoaded", function() {
         paragraph.addEventListener('click', () => {
           // Remove 'active' class from all paragraphs
           paragraphs.forEach(p => p.classList.remove('active'));
+          // Add 'active' class to the clicked paragraph
+          paragraph.classList.add('active');
+        });
+    });
+  }
+  if(sub_paragraphs){
+    sub_paragraphs.forEach(paragraph => {
+        paragraph.addEventListener('click', () => {
+          // Remove 'active' class from all paragraphs
+          sub_paragraphs.forEach(p => p.classList.remove('active'));
           // Add 'active' class to the clicked paragraph
           paragraph.classList.add('active');
         });
