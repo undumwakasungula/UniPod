ba34a4517ebf4bc3fc16f7b4573a8f97c888242b
undumwakasungula <beee22-umpeta@mubas.ac.mw>
2025-04-20 20:30:30 +0200

    Update workshop_flow.js

--- public/workshop_flow.js
+++ public/workshop_flow.js
@@ -412,8 +412,8 @@ const fetchRealTimeData = () => {
           <td>${item.Availability}</td>
           <td>
           <div class="action-buttons">
-            <button class="edit-btn" data-id="${item.id}">Edit</button>
-            <button class="delete-btn" data-id="${item.id}">Delete</button>
+            <button class="edit-btn" data-id="${item.id}"><i class="fa-solid fa-edit"></i>Edit</button>
+            <button class="delete-btn" data-id="${item.id}"><i class="fa-solid fa-trash"></i>Delete</button>
           </div>
           </td>
         </tr>
@@ -630,8 +630,8 @@ const showProjectTable = (projectsData) => {
                 <td>${item.Create_Date}</td>
                 <td>
                     <div class="action-buttons">
-                        <button class="edit-btn" data-id="${item.id}">Edit</button>
-                        <button class="delete-btn" data-id="${item.id}">Delete</button>
+                        <button class="edit-btn" data-id="${item.id}"><i class="fa-solid fa-edit"></i>Edit</button>
+                        <button class="delete-btn" data-id="${item.id}"><i class="fa-solid fa-trash"></i>Delete</button>
                     </div>
                 </td>
             <tr>
