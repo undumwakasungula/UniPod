6128dcf208043e8231ec4b56c272a478d6e5ae6c
undumwakasungula <beee22-umpeta@mubas.ac.mw>
2025-04-26 15:50:24 +0300

    Update student_membershipFlow.js

--- public/student_membershipFlow.js
+++ public/student_membershipFlow.js
@@ -63,8 +63,8 @@ document.addEventListener("DOMContentLoaded",function(){
     
     // Function to display payments in cards (instead of a table)
     const showCards = (projectsData) => {
-        const listBody = document.querySelector("#ClientProject");
-        listBody.innerHTML = ""; // Clear existing entries
+        const list_body = document.querySelector("#ClientProject");
+        list_body.innerHTML = ""; // Clear existing entries
 
         projectsData.forEach((item) => {
             const card = `
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded",function(){
                     </div>
 
             `;
-            listBody.innerHTML += card;
+            list_body.innerHTML += card;
         });
     };
 
